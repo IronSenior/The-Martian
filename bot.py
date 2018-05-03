943a114dd91d5feedc133ecd693c09a0ef95e26c
pepemarquezof <pepemarquezof@gmail.com>
2018-05-03 22:30:39 +0200

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import telebot
+#from libs.keyboard import *
+import private as tk
+import random
+import time
+
+#CONFIGURACIÓN DE TELEGRAM
+token = tk.tk()
+bot = telebot.TeleBot(token)
