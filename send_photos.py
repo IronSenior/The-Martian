f65fb979cce476948559633a4ed1778ddbe168d0
Jose Marquez <pepemarquezof@gmail.com>
2018-05-12 21:19:08 +0200

    Api implementada totalmente

--- send_photos.py
+++ send_photos.py
@@ -28,13 +28,15 @@ def send_info():
 	nasa_api = requests.get("https://api.nasa.gov/planetary/apod?api_key=mxDbxnbvRcDo3mrclKtKlUypkjdK65P80GJLmBQZ")
 	nasa_info = json.loads(nasa_api.content)
 	photo = nasa_info["url"]
+	title = nasa_info["title"]
 	print (photo)
 
 
 	for user in users:
 		bot.send_message(user, "Una foto del espacio nueva cada día")
 		bot.send_photo(user, photo)
-	time.sleep(10)#(86400)
+		bot.send_message(user, title)
+	time.sleep(86400)
 	send_info()
 
 send_info()
